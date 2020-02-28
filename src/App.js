3d94752259c68d6b8cabcbf518bea965901a3d95
leregi <leregi46@gmail.com>
2020-02-27 20:07:34 -0500

    react router guide

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
+import React, {Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { BrowserRouter as Router, Route, Link, Redirect, Switch ,useLocation } from 'react-router-dom'; 
+export default function App() {
+  const name = 'Jhon Doe';
+  const isAuthenticated = false;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Router>
+    <main>
+      <nav>
+        <ul>
+	  <li><Link to="/">Home</Link></li>
+          <li><Link to={`/about/${name}`}>About</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+        </nav>
+	 <Switch>
+            <Route path="/" exact component={Home} />
+	    { 
+		!isAuthenticated ?
+	     <>
+	    <Route path="/about/:name" component={About} />
+            <Route path="/contact" component={Contact} />
+	    <Route render={ () => <h1> 404: page not found </h1>} />
+            </> : <Redirect to="/" />
+	    }
+         </Switch>
+     </main>
+ </Router>
   );
 }
+// Home Page
+const Home = () => (
+  <Fragment>
+    <h1>Home</h1>
+    <FakeText />
+  </Fragment>
+  );
+// About Page
+const About = ({match:{params:{name}}}) => (
+  <Fragment>
+    { name !== 'Jhon Doe' ? <Redirect to='/' /> : null }
+    <h1>About {name} </h1>
+    <FakeText />
+  </Fragment>
+  );
+// Contact Page
+const Contact = ({history}) => { 
+const { pathname } = useLocation();
 
-export default App;
+return (
+  <Fragment>
+    <h1>Contact</h1>
+	  <p> Current URL: {pathname} </p>
+    <button onClick={() => history.push('/') } > Go to home </button>
+    <FakeText />
+  </Fragment>
+  )};
+
+const FakeText = () => (
+  <p>
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+  </p>
+  )
